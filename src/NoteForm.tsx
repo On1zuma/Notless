5670eed4fb91978e5f55e05e0e62400a457c29b8
w.mesfioui <w.mesfioui@optimum-automotive.com>
2023-10-30 14:56:02 +0100

    init

---
+++ src/NoteForm.tsx
@@ -0,0 +1,12 @@
+import { Form, Stack } from "react-bootstrap";
+
+export function NoteForm() {
+    return(
+        <Form>
+            <Stack gap={4}>
+                <h1>1</h1>
+                <h1>1</h1>
+            </Stack>
+        </Form>
+    )
+}
