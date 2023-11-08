3fcf67d1ec63f22ba253adefd37d22a361858048
w.mesfioui <w.mesfioui@optimum-automotive.com>
2023-11-08 16:58:58 +0100

    feat: Edit Page

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -3,28 +3,35 @@ import { Form, Stack, Col, Row, Button } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 import { NoteData, Tag } from "./App";
-import { v4 as uuidV4} from "uuid"
- 
-type NoteFormProps = {
-    onSubmit: (data: NoteData) => void
-    onAddTag: (tag: Tag) => void
-    availableTags: Tag[]
-}
+import { v4 as uuidV4 } from "uuid";
 
-export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+} & Partial<NoteData>;
+
+export function NoteForm({
+  title = "",
+  markdown = "",
+  tags = [],
+  onSubmit,
+  onAddTag,
+  availableTags,
+}: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([])
-  const navigate = useNavigate()
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
+  const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
     onSubmit({
-        title: titleRef.current!.value,
-        markdown: markdownRef.current!.value,
-        tags: selectedTags,
-    })
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: selectedTags,
+    });
     navigate("..");
   }
 
@@ -35,35 +42,46 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control ref={titleRef} required />
+              <Form.Control ref={titleRef} required defaultValue={title} />
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
               <CreatableReactSelect
-              onCreateOption={label => { 
-                const newTag = { id: uuidV4(), label}
-                onAddTag(newTag)
-                setSelectedTags(prev => [...prev, newTag])
-              }}
-              value={selectedTags.map(tag => {
-                return { label: tag.label, value:tag.id }
-              })} 
-              options={availableTags.map(tag => { return { label: tag.label, value: tag.id}})}
-              onChange={tags => {
-                setSelectedTags(tags.map(tag =>{
-                    return { label: tag.label, id: tag.value}
-                }))
-              }}
-              isMulti />
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidV4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
             </Form.Group>
           </Col>
         </Row>
 
         <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
+          <Form.Control
+            defaultValue={markdown}
+            required
+            as="textarea"
+            ref={markdownRef}
+            rows={15}
+          />
         </Form.Group>
 
         <Stack direction="horizontal" gap={2} className="justify-content-end">
